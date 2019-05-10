fbb117d55b60a4c23d28f907f785d6d8138d5098
kai-machida <49702001+kai-machida@users.noreply.github.com>
2019-05-10 13:50:13 +0900

    Initial commit

---
+++ fukusyu0510/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukusyu0510
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.ForeColor = SystemColors.Control;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = SystemColors.Control;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = true;
+        }
+    }
+}
